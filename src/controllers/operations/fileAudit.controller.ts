f06de91b4196be4f8e0c8f4b778ba306e1aa25f3
James Marques <jamzmarks@gmail.com>
2025-09-08 07:29:09 -0300

    first commit

--- src/controllers/operations/fileAudit.controller.ts
+++ src/controllers/operations/fileAudit.controller.ts
@@ -7,55 +7,13 @@ import { FileOperation } from 'generated/prisma';
 export class FileAuditController {
   constructor(private readonly fileAuditService: FileAuditService) {}
 
-  // CREATE
-  @Post()
-  async createLog(
-    @Body() body: {
-      userId: string;
-      email: string;
-      operation: FileOperation;
-      level?: LogLevel;
-      description?: string;
-      fileName?: string;
-      fileSize?: number;
-      fileType?: string;
-      folderId?: string;
-    },
-  ) {
-    return this.fileAuditService.createLog(body);
-  }
-
-  // READ ALL (opcional filtrar por userId)
-  @Get()
-  async findLogs(@Body('userId') userId?: string) {
-    return this.fileAuditService.findLogs(userId);
-  }
-
-  // READ ONE
   @Get(':id')
   async findLogById(@Param('id') id: string) {
     return this.fileAuditService.findLogById(id);
   }
 
-  // UPDATE
-  @Patch(':id')
-  async updateLog(
-    @Param('id') id: string,
-    @Body()
-    body: {
-      description?: string;
-      level?: LogLevel;
-      fileName?: string;
-      fileSize?: number;
-      fileType?: string;
-      folderId?: string;
-    },
-  ) {
-    return this.fileAuditService.updateLog(id, body);
-  }
-
-  @Delete(':id')
-  async deleteLog(@Param('id') id: string) {
-    return this.fileAuditService.deleteLog(id);
-  }
+//   @Delete(':id')
+//   async deleteLog(@Param('id') id: string) {
+//     return this.fileAuditService.deleteLog(id);
+//   }
 }
